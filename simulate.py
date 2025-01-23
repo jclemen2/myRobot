487280ff0bb2bf4d76c8338a1abb9d8ebe51eba0
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-01-23 10:51:58 -0500

    Adding sensors.

--- simulate.py
+++ simulate.py
@@ -1,5 +1,8 @@
 import pybullet as p
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
+import numpy
+import os
 import time
 
 physicsClient = p.connect(p.GUI)
@@ -9,8 +12,28 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("box.sdf")
-for i in range (1000):
+pyrosim.Prepare_To_Simulate(robotId)
+
+# Create array to store sensor values
+backLegSensorValues = numpy.zeros(100)
+frontLegSensorValues = numpy.zeros(100)
+
+for i in range (100):
     p.stepSimulation()
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     time.sleep(1/100)
-    print(i)
+
+# Define the full path to the data directory
+data_dir = "/Users/justineclement/PycharmProjects/myRobot/data"
+
+# Define the full path for the file
+file_path_backLeg = os.path.join(data_dir, "backLegSensorValues.npy")
+file_path_frontLeg = os.path.join(data_dir, "frontLegSensorValues.npy")
+
+# Save the array to the file
+numpy.save(file_path_backLeg, backLegSensorValues)
+numpy.save(file_path_frontLeg, frontLegSensorValues)
+
 p.disconnect()
+
