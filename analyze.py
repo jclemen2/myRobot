dc192bc0046cfcde7f8465f6a6e70aea581f2c0c
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-01-23 12:37:10 -0500

    Control of Movement

--- analyze.py
+++ analyze.py
@@ -3,14 +3,27 @@ import matplotlib.pyplot as plt
 
 from simulate import backLegSensorValues
 from simulate import frontLegSensorValues
+from simulate import motorTargetAngles_backLeg
+from simulate import motorTargetAngles_frontLeg
 
 # Load the sensor values from the file
 data_backLeg = numpy.load("data/backLegSensorValues.npy")
 data_frontLeg = numpy.load("data/frontLegSensorValues.npy")
+data_motorTargetAngles_backLeg = numpy.load("data/motorTargetAngles_backLeg.npy")
+data_motorTargetAngles_frontLeg = numpy.load("data/motorTargetAngles_frontLeg.npy")
 
-# Plot data
-plt.plot(backLegSensorValues, label="Back Leg", linewidth=3.5)
+# Plot sensor data
+plt.figure(1)
+plt.plot(backLegSensorValues, label="Back Leg", linewidth=5)
 plt.plot(frontLegSensorValues, label="Front Leg")
+
+# Plot motor target angle
+plt.figure(2)
+plt.plot(motorTargetAngles_frontLeg, label="Front leg motor values", linewidth=5)
+plt.plot(motorTargetAngles_backLeg, label="Back leg motor values")
+plt.title("Motor Commands")
+plt.xlabel("Steps")
+plt.ylabel("Value in Radians")
 
 # Annotate Graph
 plt.legend()
--- simulate.py
+++ simulate.py
@@ -7,6 +7,18 @@ from math import pi
 import random
 import time
 
+# Constant
+SIMULATION_TIME = 1000
+
+# Variables
+amplitude_backLeg = pi/4
+frequency_backLeg = 12
+phaseOffset_backLeg = 0
+
+amplitude_frontLeg = 0
+frequency_frontLeg = 0
+phaseOffset_frontLeg = 0
+
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
@@ -17,10 +29,16 @@ p.loadSDF("box.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
 # Create array to store sensor values
-backLegSensorValues = numpy.zeros(1000)
-frontLegSensorValues = numpy.zeros(1000)
+backLegSensorValues = numpy.zeros(SIMULATION_TIME)
+frontLegSensorValues = numpy.zeros(SIMULATION_TIME)
 
-for i in range (1000):
+# Create array to store sinusoidal motor values
+num_iterations = SIMULATION_TIME
+time_values = numpy.linspace(0, 2 * pi, num_iterations)
+motorTargetAngles_backLeg = amplitude_backLeg * numpy.sin(frequency_backLeg * time_values + phaseOffset_backLeg)
+motorTargetAngles_frontLeg = amplitude_frontLeg * numpy.sin(frequency_frontLeg * time_values + phaseOffset_frontLeg)
+
+for i in range (SIMULATION_TIME):
     p.stepSimulation()
 
     # Get sensor values
@@ -32,18 +50,18 @@ for i in range (1000):
         bodyIndex=robotId, # What robot should the motor be attached to
         jointName="Torso_BackLeg",  # What joint should the motor be attached to
         controlMode=p.POSITION_CONTROL,  # How the motor will attempt to control the motion of the joint
-        targetPosition=-pi/4.0,  # Desired position
-        maxForce=500)  # Total torque
+        targetPosition=motorTargetAngles_backLeg[i],  # Desired position
+        maxForce=100)  # Total torque
 
     pyrosim.Set_Motor_For_Joint(
         bodyIndex=robotId,  # What robot should the motor be attached to
         jointName="Torso_FrontLeg",  # What joint should the motor be attached to
         controlMode=p.POSITION_CONTROL,  # How the motor will attempt to control the motion of the joint
-        targetPosition=pi/4.0,  # Desired position
-        maxForce=500)  # Total torque
+        targetPosition=motorTargetAngles_frontLeg[i],  # Desired position
+        maxForce=100)  # Total torque
 
     # Slow the simulation
-    time.sleep(1/100)
+    time.sleep(1/240)
 
 # Define the full path to the data directory
 data_dir = "/Users/justineclement/PycharmProjects/myRobot/data"
@@ -51,10 +69,14 @@ data_dir = "/Users/justineclement/PycharmProjects/myRobot/data"
 # Define the full path for the file
 file_path_backLeg = os.path.join(data_dir, "backLegSensorValues.npy")
 file_path_frontLeg = os.path.join(data_dir, "frontLegSensorValues.npy")
+file_path_motorTargetAngles_backLeg = os.path.join(data_dir, "motorTargetAngles_backLeg.npy")
+file_path_motorTargetAngles_frontLeg = os.path.join(data_dir, "motorTargetAngles_frontLeg.npy")
 
 # Save the array to the file
 numpy.save(file_path_backLeg, backLegSensorValues)
 numpy.save(file_path_frontLeg, frontLegSensorValues)
+numpy.save(file_path_motorTargetAngles_backLeg, motorTargetAngles_backLeg)
+numpy.save(file_path_motorTargetAngles_frontLeg, motorTargetAngles_frontLeg)
 
 p.disconnect()
 
