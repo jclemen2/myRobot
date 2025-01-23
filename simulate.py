d496015e13ba5caa351fea5fff0e115c14d7f825
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-01-23 11:20:47 -0500

    No Movement

--- simulate.py
+++ simulate.py
@@ -3,6 +3,8 @@ import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy
 import os
+from math import pi
+import random
 import time
 
 physicsClient = p.connect(p.GUI)
@@ -15,13 +17,32 @@ p.loadSDF("box.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
 # Create array to store sensor values
-backLegSensorValues = numpy.zeros(100)
-frontLegSensorValues = numpy.zeros(100)
+backLegSensorValues = numpy.zeros(1000)
+frontLegSensorValues = numpy.zeros(1000)
 
-for i in range (100):
+for i in range (1000):
     p.stepSimulation()
+
+    # Get sensor values
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+
+    # Simulate motor
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex=robotId, # What robot should the motor be attached to
+        jointName="Torso_BackLeg",  # What joint should the motor be attached to
+        controlMode=p.POSITION_CONTROL,  # How the motor will attempt to control the motion of the joint
+        targetPosition=-pi/4.0,  # Desired position
+        maxForce=500)  # Total torque
+
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex=robotId,  # What robot should the motor be attached to
+        jointName="Torso_FrontLeg",  # What joint should the motor be attached to
+        controlMode=p.POSITION_CONTROL,  # How the motor will attempt to control the motion of the joint
+        targetPosition=pi/4.0,  # Desired position
+        maxForce=500)  # Total torque
+
+    # Slow the simulation
     time.sleep(1/100)
 
 # Define the full path to the data directory
