431c0132f147b07c022d8d075359d555f3a7fb5a
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-02-12 17:53:50 -0500

    Refactored.

--- simulation.py
+++ simulation.py
@@ -1,57 +1,42 @@
 from world import WORLD
 from robot import ROBOT
+
 import pybullet as p
 import pybullet_data
 import constants as c
-import pyrosim.pyrosim as pyrosim
 import time
 
-class SIMULATION:
+class SIMULATION: # name of the class
+    # Constructor
     def __init__(self):
-        # Connect to physics simulation
         self.physicsClient = p.connect(p.GUI)
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
-        p.setGravity(c.grav_x, c.grav_y, c.grav_z)
+        p.setGravity(c.GRAV_X, c.GRAV_Y, c.GRAV_Z)
 
-        # Initialize world and robot
+        # Create a world instance
         self.world = WORLD()
+
+        # Create a robot instance
         self.robot = ROBOT()
 
-        # Prepare to simulate the robot
-        pyrosim.Prepare_To_Simulate(self.robot.robotId)
-        self.robot.Prepare_To_Sense()
-
-
-    def Run(self):
-        for t in range(c.simulation_time):
-            # print(t)
-            p.stepSimulation()
-
-            # Enable sensing in the robot and pass the current time steep
-            self.robot.Sense(t)
-
-            #
-            # # Get sensor values
-            # backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-            # frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-            #
-            # # Simulate motor
-            # pyrosim.Set_Motor_For_Joint(
-            # bodyIndex=robotId, # What robot should the motor be attached to
-            # jointName="Torso_BackLeg",  # What joint should the motor be attached to
-            # controlMode=p.POSITION_CONTROL,  # How the motor will attempt to control the motion of the joint
-            # targetPosition=motorTargetAngles_backLeg[i],  # Desired position
-            # maxForce=c.force_motor_backleg)  # Total torque
-            #
-            # pyrosim.Set_Motor_For_Joint(
-            # bodyIndex=robotId,  # What robot should the motor be attached to
-            # jointName="Torso_FrontLeg",  # What joint should the motor be attached to
-            # controlMode=p.POSITION_CONTROL,  # How the motor will attempt to control the motion of the joint
-            # targetPosition=motorTargetAngles_frontLeg[i],  # Desired position
-            # maxForce=c.force_motor_frontleg)  # Total torque
-            #
-            # # Slow the simulation
-            time.sleep(c.sleep)
-
+    # Destructor
     def __del__(self):
-        p.disconnect() # Disconnect physics client when the simulation ends
+        p.disconnect()
+
+    def Run(self, steps=c.SIMULATION_TIME, time_step=c.SLEEP):
+        for t in range (steps):
+            p.stepSimulation()
+            self.robot.Sense(t)  # Robot senses environment
+            self.robot.Act(t) # Robot acts on environment
+            time.sleep(time_step)  # Slow down to visualize steps
+
+            # Slow the simulation
+            time.sleep(c.SLEEP)
+
+        # # Save sensor and motor values after simulation
+        # for sensor in self.robot.sensors.values():
+        #     sensor.Save_Values()
+        #
+        # for motor in self.robot.motors.values():
+        #     motor.Save_Values()
+
