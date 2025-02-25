c2fd9cf426338170de94cf4ccef1cf2ba23bca8a
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-02-25 15:48:43 -0500

    Finished RandomSearch.

--- generate.py
+++ generate.py
@@ -1,5 +1,6 @@
 from ctypes import c_int32
 import pyrosim.pyrosim as pyrosim
+import random
 
 def Create_World():
     # Start generating the SDF file
@@ -48,15 +49,21 @@ def Generate_Brain():
     pyrosim.Send_Motor_Neuron(name=3, jointName="Torso_BackLeg")
     pyrosim.Send_Motor_Neuron(name=4, jointName="Torso_FrontLeg")
 
-    # Create synapses
-    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3,
-                         weight=1.0) # this connects neuron 1 to neuron 3 with a synaptic with weight 1.0.
-    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=3,
-                         weight=1.0)  # this connects neuron 2 to neuron 3 with a synaptic with weight 1.0.
-    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=4,
-                         weight=0.5)  # this connects neuron 1 to neuron 4 with a synaptic with weight 1.0.
-    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4,
-                         weight=0.0)  # this connects neuron 2 to neuron 4 with a synaptic with weight 1.0.
+    # # Create synapses
+    # pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3,
+    #                      weight=1.0) # this connects neuron 1 to neuron 3 with a synaptic with weight 1.0.
+    # pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=3,
+    #                      weight=1.0)  # this connects neuron 2 to neuron 3 with a synaptic with weight 1.0.
+    # pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=4,
+    #                      weight=0.5)  # this connects neuron 1 to neuron 4 with a synaptic with weight 1.0.
+    # pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4,
+    #                      weight=0.0)  # this connects neuron 2 to neuron 4 with a synaptic with weight 1.0.
+
+    # Create synapses using nested loops
+    for sensor_neuron in range(3):  # Iterate over sensor neurons 0, 1, 2
+        for motor_neuron in range(3, 5):  # Iterate over motor neurons 3, 4
+            random_weight = random.uniform(-1,1)  # Generate a random weight between -1 and 1
+            pyrosim.Send_Synapse(sourceNeuronName=sensor_neuron, targetNeuronName=motor_neuron, weight=random_weight)
 
     # Finalize the URDF file
     pyrosim.End()
