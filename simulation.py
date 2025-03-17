be12fd07c8a0673eb10401a0d58238c288f122bc
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-17 15:14:01 -0400

    Show evolved behavior.

--- simulation.py
+++ simulation.py
@@ -8,8 +8,14 @@ import time
 
 class SIMULATION: # name of the class
     # Constructor
-    def __init__(self):
-        self.physicsClient = p.connect(p.DIRECT)
+    def __init__(self, directOrGUI):
+        # Choose the appropriate simulation mode
+        if directOrGUI == "DIRECT":
+            self.physicsClient = p.connect(p.DIRECT)  # Run in non-visual mode (faster)
+        else:
+            self.physicsClient = p.connect(p.GUI)  # Run with GUI (visual mode)
+
+        # Set up simulation environment
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
         p.setGravity(c.GRAV_X, c.GRAV_Y, c.GRAV_Z)
 
