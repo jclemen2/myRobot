4def1071f8d80e28d43d0d411d2fee172fe9d6a4
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-17 13:48:25 -0400

    Evaluate.

--- hillclimber.py
+++ hillclimber.py
@@ -3,3 +3,6 @@ from solution import SOLUTION
 class HILL_CLIMBER: # name of the class
     def __init__(self):
         self.parent = SOLUTION()
+
+    def Evolve(self):
+        self.parent.Evaluate()
