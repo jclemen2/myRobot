4b91679ff82b7f38d7b319a273b15eaa26ba5259
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-17 13:27:00 -0400

    Creating Hill CLimber.

--- search.py
+++ search.py
@@ -1,16 +1,18 @@
+from hillclimber import HILL_CLIMBER
 import os
 
-def main():
-    # Loop to generate and simulate robots twice
-    for i in range(5):
-        print(f"Iteration {i + 1}")
+hc = HILL_CLIMBER()
 
-        # Execute generate.py
-        os.system("python3 generate.py")
-
-        # Execute simulate.py
-        os.system("python3 simulate.py")
-
-if __name__ == "__main__":
-    main()
-
+# def main():
+#     # Loop to generate and simulate robots twice
+#     for i in range(5):
+#         print(f"Iteration {i + 1}")
+#
+#         # Execute generate.py
+#         os.system("python3 generate.py")
+#
+#         # Execute simulate.py
+#         os.system("python3 simulate.py")
+#
+# if __name__ == "__main__":
+#     main()
