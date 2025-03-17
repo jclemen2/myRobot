41e5e6bf64d63d5356caa8f9d0c73a1dfb693f8b
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-17 14:53:53 -0400

    Running blind.

--- robot.py
+++ robot.py
@@ -45,7 +45,7 @@ class ROBOT: # name of the class
 
     def Think(self):
         self.nn.Update()
-        self.nn.Print()
+        #self.nn.Print()
 
     def Get_Fitness(self):
         stateOfLinkZero = p.getLinkState(self.robotId,0)
