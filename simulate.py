083d6f6f9c5e88046eee0c4a9f2ddc7e3fd452f4
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-17 15:14:10 -0400

    Show evolved behavior.

--- simulate.py
+++ simulate.py
@@ -1,6 +1,10 @@
 from simulation import SIMULATION
+import sys
+
+# Extract command-line argument
+directOrGUI = sys.argv[1] if len(sys.argv) > 1 else "DIRECT"
 
 # Create a simulation instance
-simulation = SIMULATION()
+simulation = SIMULATION(directOrGUI)
 simulation.Run()
 simulation.Get_Fitness()
