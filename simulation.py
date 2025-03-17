dfcd09c06469d2f783fd836e5053f24413b0c5e3
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-17 14:02:26 -0400

    Writing fitness.

--- simulation.py
+++ simulation.py
@@ -40,3 +40,6 @@ class SIMULATION: # name of the class
         #
         # for motor in self.robot.motors.values():
         #     motor.Save_Values()
+
+    def Get_Fitness(self):
+        self.robot.Get_Fitness()
