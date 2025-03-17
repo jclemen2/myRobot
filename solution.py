0883a28dc9bf9e818709792894b4b2b85062ef10
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-17 13:27:41 -0400

    Creating Hill CLimber.

---
+++ solution.py
@@ -0,0 +1,9 @@
+import numpy
+
+class SOLUTION: # name of the class
+    def __init__(self):
+        # Generate a 3-row x 2-column matrix with random values in [0,1]
+        self.weights = numpy.random.rand(3, 2)
+
+        # Scale to [-1,1]
+        self.weights = self.weights * 2 - 1
