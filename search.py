db37b59349eb8e1d6b9fbfccc4b7b535477cba04
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-17 15:13:43 -0400

    Show evolved behavior.

--- search.py
+++ search.py
@@ -1,8 +1,14 @@
 from hillclimber import HILL_CLIMBER
 import os
 
+# Create an instance of HILL_CLIMBER
 hc = HILL_CLIMBER()
+
+# Run the evolutionary process
 hc.Evolve()
+
+# Show the best solution in GUI mode
+hc.Show_Best()
 
 # def main():
 #     # Loop to generate and simulate robots twice
