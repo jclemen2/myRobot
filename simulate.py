fdadb8a74103de589d4d7326187a4fd44b5bd0e9
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-20 14:24:51 -0400

    Finished HillClimber.

--- simulate.py
+++ simulate.py
@@ -1,6 +1,9 @@
 from simulation import SIMULATION
+import sys
+
+directOrGUI = sys.argv[1] #extract command-line argument to determine mode (GUI or direct)
 
 # Create a simulation instance
-simulation = SIMULATION()
+simulation = SIMULATION(directOrGUI)
 simulation.Run()
 simulation.Get_Fitness()
