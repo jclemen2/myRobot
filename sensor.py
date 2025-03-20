41ce9f243f6633e86370683fdb58ad763c18bbba
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-20 14:46:09 -0400

    Beginning PHC.

--- sensor.py
+++ sensor.py
@@ -1,17 +1,36 @@
-import pyrosim.pyrosim as pyrosim
 import numpy
+import pyrosim.pyrosim as pyrosim
 import constants as c
+import os
 
-class SENSOR:
-    def __init__(self, linkName):
+# Define the full path to the data directory
+data_dir = "/Users/justineclement/PycharmProjects/myRobot/data"
+os.makedirs(data_dir, exist_ok=True)
+
+class SENSOR: # name of the class
+    def __init__(self, linkName):  # Constructor
         self.linkName = linkName
+        # Create array to store sensor values
+        self.values = numpy.zeros(c.SIMULATION_TIME)
 
-        # create a numpy vector filled with zeros for sensor values
-        self.values = numpy.zeros(c.num_iterations)
-
-    def Get_value (self, t):
-        #sensor value for back leg
+    def Get_Value(self, t):
+        # Get sensor values
         self.values[t] = pyrosim.Get_Touch_Sensor_Value_For_Link(self.linkName)
+
+    def Save_Values(self):
+        """Saves sensor values to a file."""
+        file_path = os.path.join(data_dir, f"{self.linkName}SensorValues.npy")
+        numpy.save(file_path, self.values)
+        #print(f"Saved sensor data: {file_path}")
+
+
+
+
+
+
+
+
+
 
 
 
