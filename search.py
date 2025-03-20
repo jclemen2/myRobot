4ded4a63b2dfcf7cd7eed2653193bb1a82300784
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-20 10:48:24 -0400

    New randomsearch.

--- search.py
+++ search.py
@@ -1,16 +1,7 @@
 import os
 
-def main():
-    # Loop to generate and simulate robots twice
-    for i in range(5):
-        print(f"Iteration {i + 1}")
-
-        # Execute generate.py
-        os.system("python3 generate.py")
-
-        # Execute simulate.py
-        os.system("python3 simulate.py")
-
-if __name__ == "__main__":
-    main()
+robot = 5
+for i in range(robot):
+    os.system("python3 generate.py")
+    os.system("python3 simulate.py")
 
