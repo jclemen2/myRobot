4d8fd19f12fa686a6d9883cb83a58cc89d38c8dd
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-20 10:58:56 -0400

    Beginning HillClimber.

--- search.py
+++ search.py
@@ -1,25 +1,6 @@
-from hillclimber import HILL_CLIMBER
 import os
 
-# Create an instance of HILL_CLIMBER
-hc = HILL_CLIMBER()
-
-# Run the evolutionary process
-hc.Evolve()
-
-# Show the best solution in GUI mode
-hc.Show_Best()
-
-# def main():
-#     # Loop to generate and simulate robots twice
-#     for i in range(5):
-#         print(f"Iteration {i + 1}")
-#
-#         # Execute generate.py
-#         os.system("python3 generate.py")
-#
-#         # Execute simulate.py
-#         os.system("python3 simulate.py")
-#
-# if __name__ == "__main__":
-#     main()
+robot = 5
+for i in range(robot):
+    os.system("python generate.py")
+    os.system("python simulate.py")
