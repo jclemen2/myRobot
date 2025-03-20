f59a13c8bf7648d647144503e09eebbbb7091a03
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-20 13:10:00 -0400

    Reading Fitness.

--- hillclimber.py
+++ hillclimber.py
@@ -1,6 +1,7 @@
 from solution import SOLUTION
 
 import constants as c
+import copy
 
 class HILL_CLIMBER:
     def __init__(self):
@@ -20,7 +21,7 @@ class HILL_CLIMBER:
         self.Select()
 
     def Spawn(self):
-        pass
+        self.child = copy.deepcopy(self.parent)
 
     def Mutate(self):
         pass
