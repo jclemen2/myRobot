1ffe33158b6ec72b5466d66964a9befa9450d2cf
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-20 14:45:23 -0400

    Beginning PHC.

--- search.py
+++ search.py
@@ -1,10 +1,12 @@
+from hillclimber import HILL_CLIMBER
 import os
-from parallelHillClimber import PARALLEL_HILL_CLIMBER
 
-#robot = 5
-#for i in range(robot):
-#    os.system("python generate.py")
-#    os.system("python simulate.py")
-phc = PARALLEL_HILL_CLIMBER ()
-phc.Evolve()
-phc.Show_Best()
+hc = HILL_CLIMBER()
+
+hc.Evolve()
+hc.Show_Best()
+
+# robot = 5
+# for i in range(robot):
+#     os.system("python generate.py")
+#     os.system("python simulate.py")
