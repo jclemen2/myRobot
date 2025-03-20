9d6a56d56888470c04f603048670856b7277ff4b
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-20 15:56:37 -0400

    Modifying brain files.

--- simulate.py
+++ simulate.py
@@ -2,9 +2,10 @@ from simulation import SIMULATION
 import sys
 
 directOrGUI = sys.argv[1] #extract command-line argument to determine mode (GUI or direct)
+solutionID = sys.argv[2] # The unique solution ID
 
 # Create a simulation instance
-simulation = SIMULATION(directOrGUI)
+simulation = SIMULATION(directOrGUI, solutionID)
 simulation.Run()
 simulation.Get_Fitness()
 
