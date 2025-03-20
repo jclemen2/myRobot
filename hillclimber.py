3bd53ddccfd7e22e87b43fc39819e0babec18bd6
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-20 13:06:44 -0400

    Reading Fitness.

--- hillclimber.py
+++ hillclimber.py
@@ -1,5 +1,7 @@
 from solution import SOLUTION
 
+import constants as c
+
 class HILL_CLIMBER:
     def __init__(self):
         self.parent = SOLUTION()
@@ -7,3 +9,21 @@ class HILL_CLIMBER:
     def Evolve(self):
         self.parent.Evaluate()
 
+        for currentGeneration in range(c.numberOfGenerations):
+            self.Evolve_For_One_Generation()
+
+
+    def Evolve_For_One_Generation(self):
+        self.Spawn()
+        self.Mutate()
+        self.child.Evaluate()
+        self.Select()
+
+    def Spawn(self):
+        pass
+
+    def Mutate(self):
+        pass
+
+    def Select(self):
+        pass
