14a73e0681fe48d47806caed89e76eef3e9b84ac
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-20 12:51:07 -0400

    Writing fitness.

--- robot.py
+++ robot.py
@@ -47,10 +47,16 @@ class ROBOT: # name of the class
         self.nn.Update()
         #self.nn.Print()
 
+
     def Get_Fitness(self):
-        stateOfLinkZero = p.getLinkState(self.robotId,0)
+        stateOfLinkZero = p.getLinkState(self.robotId, 0)
         positionOfLinkZero = stateOfLinkZero[0]
         xCoordinateOfLinkZero = positionOfLinkZero[0]
 
-        with open("fitness.txt", "w") as f:
+        with open("fitness.txt", "w") as f:  # "w" mode overwrites the file, we want to write the fitness to a txt file
             f.write(str(xCoordinateOfLinkZero))  # Write as string
+
+
+
+
+
