a8af8a967b5150da4248437b1622c0c654c274e1
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-20 10:14:45 -0400

    Meg's.

--- search.py
+++ search.py
@@ -1,25 +1,10 @@
-from hillclimber import HILL_CLIMBER
 import os
+from parallelHillClimber import PARALLEL_HILL_CLIMBER
 
-# Create an instance of HILL_CLIMBER
-hc = HILL_CLIMBER()
-
-# Run the evolutionary process
-hc.Evolve()
-
-# Show the best solution in GUI mode
-hc.Show_Best()
-
-# def main():
-#     # Loop to generate and simulate robots twice
-#     for i in range(5):
-#         print(f"Iteration {i + 1}")
-#
-#         # Execute generate.py
-#         os.system("python3 generate.py")
-#
-#         # Execute simulate.py
-#         os.system("python3 simulate.py")
-#
-# if __name__ == "__main__":
-#     main()
+#robot = 5
+#for i in range(robot):
+#    os.system("python generate.py")
+#    os.system("python simulate.py")
+phc = PARALLEL_HILL_CLIMBER ()
+phc.Evolve()
+phc.Show_Best()
