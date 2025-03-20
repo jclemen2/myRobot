3fb50b8643e35a086d1125e2a7fb9659dd824d8d
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-20 10:59:11 -0400

    Beginning HillClimber.

--- simulate.py
+++ simulate.py
@@ -1,10 +1,5 @@
 from simulation import SIMULATION
-import sys
-
-# Extract command-line argument
-directOrGUI = sys.argv[1] if len(sys.argv) > 1 else "DIRECT"
 
 # Create a simulation instance
-simulation = SIMULATION(directOrGUI)
+simulation = SIMULATION()
 simulation.Run()
-simulation.Get_Fitness()
