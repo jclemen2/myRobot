20f5b91db87521b08134d19065719f42e5c08074
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-27 14:21:31 -0400

    Running blind..

--- pyrosim/neuralNetwork.py
+++ pyrosim/neuralNetwork.py
@@ -19,14 +19,15 @@ class NEURAL_NETWORK:
         f.close()
 
     def Print(self):
+        pass
 
-        self.Print_Sensor_Neuron_Values()
+        # self.Print_Sensor_Neuron_Values()
 
-        self.Print_Hidden_Neuron_Values()
+        # self.Print_Hidden_Neuron_Values()
 
-        self.Print_Motor_Neuron_Values()
+        # self.Print_Motor_Neuron_Values()
 
-        print("")
+        #print("")
 
     def Update(self):
         for neuronName in self.neurons:
@@ -85,7 +86,7 @@ class NEURAL_NETWORK:
 
     def Print_Sensor_Neuron_Values(self):
 
-        print("sensor neuron values: " , end = "" )
+        # print("sensor neuron values: " , end = "" )
 
         for neuronName in sorted(self.neurons):
 
@@ -97,7 +98,7 @@ class NEURAL_NETWORK:
 
     def Print_Hidden_Neuron_Values(self):
 
-        print("hidden neuron values: " , end = "" )
+        # print("hidden neuron values: " , end = "" )
 
         for neuronName in sorted(self.neurons):
 
@@ -109,7 +110,7 @@ class NEURAL_NETWORK:
 
     def Print_Motor_Neuron_Values(self):
 
-        print("motor neuron values: " , end = "" )
+        # print("motor neuron values: " , end = "" )
 
         for neuronName in sorted(self.neurons):
 
