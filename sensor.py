78ab3bd24dea4fd170b82669204df72870b82ead
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-27 13:26:06 -0400

    Creating Hill Climber.

--- sensor.py
+++ sensor.py
@@ -22,12 +22,3 @@ class SENSOR: # name of the class
         file_path = os.path.join(data_dir, f"{self.linkName}SensorValues.npy")
         numpy.save(file_path, self.values)
         #print(f"Saved sensor data: {file_path}")
-
-
-
-
-
-
-
-
-
