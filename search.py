5e4e162a02a26659a087b48008caddac7ad1696e
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-27 13:25:57 -0400

    Creating Hill Climber.

--- search.py
+++ search.py
@@ -1,12 +1,11 @@
 from hillclimber import HILL_CLIMBER
+
 import os
 
 hc = HILL_CLIMBER()
 
-hc.Evolve()
-hc.Show_Best()
-
 # robot = 5
 # for i in range(robot):
-#     os.system("python generate.py")
-#     os.system("python simulate.py")
+#     os.system("python3 generate.py")
+#     os.system("python3 simulate.py")
+
