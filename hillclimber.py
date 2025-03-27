c96f449e70b701401b29877abe52300c57e8c42d
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-27 15:11:35 -0400

    Finished Parallel Hill CLimber

--- hillclimber.py
+++ hillclimber.py
@@ -32,7 +32,7 @@ class HILL_CLIMBER:
 
     def Select(self):
         if self.child.fitness < self.parent.fitness:
-            self.parent = self.child  # if the child is better replace the parent value with the child value
+                self.parent = self.child
 
     def Print(self):
         print(f"Parent Fitness: {self.parent.fitness}, Child Fitness: {self.child.fitness}")
@@ -40,6 +40,9 @@ class HILL_CLIMBER:
     def Show_Best(self):
         print("\nRe-evaluating the best solution with GUI...")
         self.parent.Evaluate("GUI")  # show the best evolved solution
+
+
+
 
 
 
