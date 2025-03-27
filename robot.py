03dcad7eee90855b823dc3224ba8da74f855d22d
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-27 13:25:47 -0400

    Creating Hill Climber.

--- robot.py
+++ robot.py
@@ -45,20 +45,7 @@ class ROBOT: # name of the class
 
     def Think(self):
         self.nn.Update()
-        #self.nn.Print()
-
-
-    def Get_Fitness(self):
-        stateOfLinkZero = p.getLinkState(self.robotId, 0)
-        positionOfLinkZero = stateOfLinkZero[0]
-        xCoordinateOfLinkZero = positionOfLinkZero[0]
-
-        # with open("fitness.txt", "w") as f:  # "w" mode overwrites the file, we want to write the fitness to a txt file
-        #     f.write(str(xCoordinateOfLinkZero))  # Write as string
-
-        f = open("fitness.txt", "w")
-        f.write(str(xCoordinateOfLinkZero))
-        f.close()
+        self.nn.Print()
 
 
 
