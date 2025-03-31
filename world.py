fa6ce9b618b235c75886df8036828dbc28492556
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-31 18:51:07 -0400

    Milestone 1.

--- world.py
+++ world.py
@@ -15,12 +15,3 @@ class WORLD:
 
         # Load the world with additional objects
         p.loadSDF("world.sdf")
-
-
-
-# import pybullet as p
-#
-# class WORLD: # name of the class
-#     def __init__(self):
-#         self.planeId = p.loadURDF("plane.urdf")# Constructor
-#         p.loadSDF("world.sdf")
