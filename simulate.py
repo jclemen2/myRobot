54efa50ce7d81eee7b5cfb4007ad2d0618303e65
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-31 18:50:29 -0400

    Milestone 1.

--- simulate.py
+++ simulate.py
@@ -24,15 +24,3 @@ simulation.Run()  # Call the Run method to start the simulation
 
 # Import from simulation
 simulation.Get_Fitness()  # This will call the Get_Fitness() method
-
-
-# from simulation import SIMULATION
-# import sys
-#
-# directOrGUI = sys.argv[1] #extract command-line argument to determine mode (GUI or direct)
-# solutionID = sys.argv[2] # The unique solution ID
-#
-# # Create a simulation instance
-# simulation = SIMULATION(directOrGUI, solutionID)
-# simulation.Run()
-# simulation.Get_Fitness()
