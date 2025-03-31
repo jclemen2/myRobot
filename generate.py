60975d9d3030764a6ccf4c2bddb467bc0b79ab92
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-31 13:14:59 -0400

    Finished quadruped

--- generate.py
+++ generate.py
@@ -1,75 +1,154 @@
-from ctypes import c_int32
+# Import pyrosim
 import pyrosim.pyrosim as pyrosim
 import random
 
 def Create_World():
-    # Start generating the SDF file
+    # Tell pyrosim the name of the file where the world will be stored
     pyrosim.Start_SDF("world.sdf")
 
-    # set variables size and position
+    # Store box position and parameters
+    pyrosim.Send_Cube(name="Box", pos=[-10,0,0.5], size=[1,1,1])
+
+    # End the SDF generation
+    pyrosim.End()
+
+
+def Generate_Body():
+    # Start creating the robot description in URDF format
+    pyrosim.Start_URDF("body.urdf")
+
+    # Block dimensions
     length = 1
     width = 1
     height = 1
-    x = 4
-    y = 2
-    z = 0.5
 
-    # Create Object
-    pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+    # Create the root cube (Torso)
+    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 1.5], size=[length, width, height])
 
-    # Finalize the SDF file
+    # Create Joint between Torso and BackLeg
+    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[0.5, 0, 1])
+
+    # Create BackLeg
+    pyrosim.Send_Cube(name="BackLeg", pos=[0.5, 0, -0.5], size=[length, width, height])
+
+    # Create joint between Torso and FrontLeg
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[-0.5, 0, 1])
+
+    # Create FrontLeg
+    pyrosim.Send_Cube(name="FrontLeg", pos=[-0.5, 0, -0.5], size=[length, width, height])
+
+    # End the URDF generation
     pyrosim.End()
 
-def Generate_Body():
-    # Start generating the URDF file
-    pyrosim.Start_URDF("body.urdf")
+    # CAN ADD IN WORLD.SDF IF NEED TO ADD THE EXTRA BOX BUT IT IS BUILT INTO WORLD FILE ALREADY
 
-    pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5], size=[1,1,1])
-    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[1, 0, 1])
-    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5],
-                      size=[1,1,1])
-    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[2, 0, 1])
-    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5],
-                      size=[1,1,1])
 
-    # Finalize the URDF file
-    pyrosim.End()
 
 def Generate_Brain():
-    # Start generating the URDF file
+    # Start creating the brain neural network
     pyrosim.Start_NeuralNetwork("brain.nndf")
 
-    # Name neurons with numbers
-    # Create sensor neurons
     pyrosim.Send_Sensor_Neuron(name=0, linkName="Torso")
     pyrosim.Send_Sensor_Neuron(name=1, linkName="BackLeg")
     pyrosim.Send_Sensor_Neuron(name=2, linkName="FrontLeg")
 
-    # Create motor neurons
     pyrosim.Send_Motor_Neuron(name=3, jointName="Torso_BackLeg")
     pyrosim.Send_Motor_Neuron(name=4, jointName="Torso_FrontLeg")
 
-    # Create synapses
-    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3,
-                         weight=1.0) # this connects neuron 1 to neuron 3 with a synaptic with weight 1.0.
-    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=3,
-                         weight=1.0)  # this connects neuron 2 to neuron 3 with a synaptic with weight 1.0.
-    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=4,
-                         weight=0.5)  # this connects neuron 1 to neuron 4 with a synaptic with weight 1.0.
-    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4,
-                         weight=0.0)  # this connects neuron 2 to neuron 4 with a synaptic with weight 1.0.
+    # Generate a synapse
+    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=1)
+    # Generate second synapse
+    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=3, weight=1)
 
-    # Create synapses using nested loops
-    for sensor_neuron in range(3):  # Iterate over sensor neurons 0, 1, 2
-        for motor_neuron in range(3, 5):  # Iterate over motor neurons 3, 4
-            random_weight = random.uniform(-1,1)  # Generate a random weight between -1 and 1
-            pyrosim.Send_Synapse(sourceNeuronName=sensor_neuron, targetNeuronName=motor_neuron, weight=1)
+    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=4, weight=0.5)
+    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4, weight=0.0)
 
-    # Finalize the URDF file
+    # Generate synapses using nested loops
+    for i in range(3):  # Sensor neurons (0, 1, 2)
+        for j in range(3, 5):  # Motor neurons (3, 4)
+            weight = random.uniform(-1, 1)  # Generate a random weight in [-1,1]
+            pyrosim.Send_Synapse(sourceNeuronName=i, targetNeuronName=j, weight=weight)
+
+    # End the URDF generation
     pyrosim.End()
 
+#Create_World()
+#Generate_Body()
+#Generate_Brain()
 
-Create_World()
-#Create_Robot()
-Generate_Body()
-Generate_Brain()
+
+# from ctypes import c_int32
+# import pyrosim.pyrosim as pyrosim
+# import random
+#
+# def Create_World():
+#     # Start generating the SDF file
+#     pyrosim.Start_SDF("world.sdf")
+#
+#     # set variables size and position
+#     length = 1
+#     width = 1
+#     height = 1
+#     x = 4
+#     y = 2
+#     z = 0.5
+#
+#     # Create Object
+#     pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+#
+#     # Finalize the SDF file
+#     pyrosim.End()
+#
+# def Generate_Body():
+#     # Start generating the URDF file
+#     pyrosim.Start_URDF("body.urdf")
+#
+#     pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5], size=[1,1,1])
+#     pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[1, 0, 1])
+#     pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5],
+#                       size=[1,1,1])
+#     pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[2, 0, 1])
+#     pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5],
+#                       size=[1,1,1])
+#
+#     # Finalize the URDF file
+#     pyrosim.End()
+#
+# def Generate_Brain():
+#     # Start generating the URDF file
+#     pyrosim.Start_NeuralNetwork("brain.nndf")
+#
+#     # Name neurons with numbers
+#     # Create sensor neurons
+#     pyrosim.Send_Sensor_Neuron(name=0, linkName="Torso")
+#     pyrosim.Send_Sensor_Neuron(name=1, linkName="BackLeg")
+#     pyrosim.Send_Sensor_Neuron(name=2, linkName="FrontLeg")
+#
+#     # Create motor neurons
+#     pyrosim.Send_Motor_Neuron(name=3, jointName="Torso_BackLeg")
+#     pyrosim.Send_Motor_Neuron(name=4, jointName="Torso_FrontLeg")
+#
+#     # Create synapses
+#     pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3,
+#                          weight=1.0) # this connects neuron 1 to neuron 3 with a synaptic with weight 1.0.
+#     pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=3,
+#                          weight=1.0)  # this connects neuron 2 to neuron 3 with a synaptic with weight 1.0.
+#     pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=4,
+#                          weight=0.5)  # this connects neuron 1 to neuron 4 with a synaptic with weight 1.0.
+#     pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4,
+#                          weight=0.0)  # this connects neuron 2 to neuron 4 with a synaptic with weight 1.0.
+#
+#     # Create synapses using nested loops
+#     for sensor_neuron in range(3):  # Iterate over sensor neurons 0, 1, 2
+#         for motor_neuron in range(3, 5):  # Iterate over motor neurons 3, 4
+#             random_weight = random.uniform(-1,1)  # Generate a random weight between -1 and 1
+#             pyrosim.Send_Synapse(sourceNeuronName=sensor_neuron, targetNeuronName=motor_neuron, weight=1)
+#
+#     # Finalize the URDF file
+#     pyrosim.End()
+#
+#
+# Create_World()
+# #Create_Robot()
+# Generate_Body()
+# Generate_Brain()
