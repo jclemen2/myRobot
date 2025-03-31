a7977e438f282fa20d023a3c773d19588de4bf39
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-03-31 13:16:01 -0400

    Finished quadruped

--- simulate.py
+++ simulate.py
@@ -1,12 +1,38 @@
+# -*- coding: utf-8 -*-
+# Cole Richardson
+
+# Imports
+import pybullet_data
+import pybullet as p
+import time
+import numpy
+# Import pyrosim
+import pyrosim.pyrosim as pyrosim
+# Import os
+import os
+# Import constants and simulation file
+import constants as c
 from simulation import SIMULATION
 import sys
 
-directOrGUI = sys.argv[1] #extract command-line argument to determine mode (GUI or direct)
-solutionID = sys.argv[2] # The unique solution ID
+# Extract the argument
+directOrGUI = sys.argv[1]
+solutionID = int(sys.argv[2])
 
-# Create a simulation instance
-simulation = SIMULATION(directOrGUI, solutionID)
-simulation.Run()
-simulation.Get_Fitness()
+simulation = SIMULATION(directOrGUI, solutionID)  # Create an instance of the SIMULATION class
+simulation.Run()  # Call the Run method to start the simulation
+
+# Import from simulation
+simulation.Get_Fitness()  # This will call the Get_Fitness() method
 
 
+# from simulation import SIMULATION
+# import sys
+#
+# directOrGUI = sys.argv[1] #extract command-line argument to determine mode (GUI or direct)
+# solutionID = sys.argv[2] # The unique solution ID
+#
+# # Create a simulation instance
+# simulation = SIMULATION(directOrGUI, solutionID)
+# simulation.Run()
+# simulation.Get_Fitness()
