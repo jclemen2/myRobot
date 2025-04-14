355dda974e1f66d206e7c719cc439146e8c0132a
jclemen2 <98429837+jclemen2@users.noreply.github.com>
2025-04-14 12:59:14 -0400

    Milestone 3.

--- simulate.py
+++ simulate.py
@@ -1,17 +1,3 @@
-# -*- coding: utf-8 -*-
-# Cole Richardson
-
-# Imports
-import pybullet_data
-import pybullet as p
-import time
-import numpy
-# Import pyrosim
-import pyrosim.pyrosim as pyrosim
-# Import os
-import os
-# Import constants and simulation file
-import constants as c
 from simulation import SIMULATION
 import sys
 
